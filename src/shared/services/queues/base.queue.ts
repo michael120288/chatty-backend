70b080fe8065edb1096bf791a1e8061b2919a99f
michael120288 <Michael.sheptun@gmail.com>
2024-08-10 22:04:26 -0400

    feat: implemented post reactions feature"

--- src/shared/services/queues/base.queue.ts
+++ src/shared/services/queues/base.queue.ts
@@ -8,7 +8,8 @@ import { IAuthJob } from '@auth/interfaces/auth.interface';
 import { IEmailJob } from '@user/interfaces/user.interface';
 import { IPostJobData } from '@post/interfaces/post.interface';
 import { IReactionJob } from '@reaction/interfaces/reaction.interface';
-type IBaseJobData = IAuthJob | IEmailJob | IPostJobData | IReactionJob;
+import { ICommentJob } from '@comment/interfaces/comment.interface';
+type IBaseJobData = IAuthJob | IEmailJob | IPostJobData | IReactionJob | ICommentJob
 
 
 let bullAdapters: BullAdapter[] = [];
