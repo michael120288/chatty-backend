4096bf37522b66224f7cad6f98a9bf1634c24838
michael120288 <78063023+michael120288@users.noreply.github.com>
2024-09-23 00:54:28 -0400

    Feature/users feature (#10)
    * add schemes to user
    
    * add shuffle and escapeRegex functions
    
    * add functions
    
    * add get all users
    
    * add get all users
    
    * add followers
    
    * add test for get-profile

--- src/shared/globals/helpers/helpers.ts
+++ src/shared/globals/helpers/helpers.ts
@@ -3,18 +3,16 @@ export class Helpers {
     const valueString = str.toLowerCase();
     return valueString
       .split(' ')
-      .map(
-        (value: string) =>
-          `${value.charAt(0).toUpperCase()}${value.slice(1).toLowerCase()}`,
-      )
+      .map((value: string) => `${value.charAt(0).toUpperCase()}${value.slice(1).toLowerCase()}`)
       .join(' ');
   }
+
   static lowerCase(str: string): string {
     return str.toLowerCase();
   }
 
   static generateRandomIntegers(integerLength: number): number {
-    const characters = '1234567890';
+    const characters = '0123456789';
     let result = ' ';
     const charactersLength = characters.length;
     for (let i = 0; i < integerLength; i++) {
@@ -23,6 +21,7 @@ export class Helpers {
     return parseInt(result, 10);
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   static parseJson(prop: string): any {
     try {
       JSON.parse(prop);
@@ -32,8 +31,20 @@ export class Helpers {
     return JSON.parse(prop);
   }
 
-  static isDataURL(value:string):boolean{
+  static isDataURL(value: string): boolean {
     const dataUrlRegex = /^\s*data:([a-z]+\/[a-z0-9-+.]+(;[a-z-]+=[a-z0-9-]+)?)?(;base64)?,([a-z0-9!$&',()*+;=\-._~:@\\/?%\s]*)\s*$/i;
     return dataUrlRegex.test(value);
+  }
+
+  static shuffle(list: string[]): string[] {
+    for (let i = list.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [list[i], list[j]] = [list[j], list[i]];
+    }
+    return list;
+  }
+
+  static escapeRegex(text: string): string {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
   }
 }
