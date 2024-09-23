169b1ddc8ef53bbdf978e68607185046d9d06711
michael120288 <78063023+michael120288@users.noreply.github.com>
2024-09-22 21:25:45 -0400

    feature:/ create feature chat (#9)

---
+++ src/features/chat/interfaces/conversation.interface.ts
@@ -0,0 +1,8 @@
+import mongoose, {Document} from 'mongoose'
+
+export interface IConversationDocument extends Document {
+  _id: mongoose.Types.ObjectId;
+  senderId: mongoose.Types.ObjectId;
+  receiverId: mongoose.Types.ObjectId;
+}
+
