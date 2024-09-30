d6799b609b2f74d8c83aed093569d74d7fcedde7
michael120288 <78063023+michael120288@users.noreply.github.com>
2024-09-30 00:43:00 -0400

    Feature/users feature (#11)
    * add schemes to user
    
    * add shuffle and escapeRegex functions
    
    * add functions
    
    * add get all users
    
    * add get all users
    
    * add followers
    
    * add test for get-profile
    
    * feat:added user controller

---
+++ src/features/user/controllers/search-user.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+import HTTP_STATUS from 'http-status-codes';
+import { Helpers } from '@global/helpers/helpers';
+import { userService } from '@service/db/user.service';
+import { ISearchUser } from '@user/interfaces/user.interface';
+
+export class Search {
+  public async user(req: Request, res: Response): Promise<void> {
+    console.log('=========================');
+    const regex = new RegExp(Helpers.escapeRegex(req.params.query), 'i');
+    const users: ISearchUser[] = await userService.searchUsers(regex);
+    console.log(users,'=========================');
+    res.status(HTTP_STATUS.OK).json({ message: 'Search results', search: users });
+  }
+}
