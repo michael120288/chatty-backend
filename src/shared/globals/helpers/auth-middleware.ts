4a4fd3c45b6437cad027e6b5293d455f2404c080
michael120288 <78063023+michael120288@users.noreply.github.com>
2024-11-03 11:55:10 -0500

    merge develop to staging  (#12)
    * add eslintignore file
    
    * feat: implement auth feature
    
    * feat: implement password reset feature with unit tests (#3)
    
    * Feature/password reset feature (#4)
    
    * feat: implement password reset feature with unit tests
    
    * feat: added post feature
    
    * add script and ts congig changes
    
    * feat: implement post reactions feature (#5)
    
    * feat: implemented post reactions feature" (#6)
    
    * Feature/post comments/follow feature (#7)
    
    * feat: implemented post reactions feature"
    
    * feat: implemented follow,unfollow,block and unlock featues
    
    * add notification feature
    
    * feat: images (#8)
    
    * feature:/ create feature chat (#9)
    
    * Feature/users feature (#10)
    
    * add schemes to user
    
    * add shuffle and escapeRegex functions
    
    * add functions
    
    * add get all users
    
    * add get all users
    
    * add followers
    
    * add test for get-profile
    
    * Feature/users feature (#11)
    
    * add schemes to user
    
    * add shuffle and escapeRegex functions
    
    * add functions
    
    * add get all users
    
    * add get all users
    
    * add followers
    
    * add test for get-profile
    
    * feat:added user controller
    
    * feature: implemented user feature
    
    * add post video
    
    * create seed script and setup npm start command

---
+++ src/shared/globals/helpers/auth-middleware.ts
@@ -0,0 +1,39 @@
+import { Request, Response, NextFunction } from 'express';
+import JWT from 'jsonwebtoken';
+import { config } from '@root/config';
+import { NotAuthorizedError } from '@global/helpers/error-handler';
+import { AuthPayload } from '@auth/interfaces/auth.interface';
+
+export class AuthMiddleware {
+  public verifyUser(req: Request, _res: Response, next: NextFunction): void {
+    //console.log(req)
+    if (!req.session?.jwt) {
+      console.log(req.session);
+      throw new NotAuthorizedError('Token is not valid.Please login again');
+    }
+
+    try {
+      const payload: AuthPayload = JWT.verify(
+        req.session?.jwt,
+        config.JWT_TOKEN!,
+      ) as AuthPayload;
+      req.currentUser = payload;
+    } catch (error) {
+      throw new NotAuthorizedError('Token is invalid.');
+    }
+    next();
+  }
+  public checkAuthentication(
+    req: Request,
+    _res: Response,
+    next: NextFunction,
+  ): void {
+    if (!req.currentUser) {
+      throw new NotAuthorizedError(
+        'User is not authenticated.Please login again',
+      );
+    }
+    next();
+  }
+}
+export const authMiddleware: AuthMiddleware = new AuthMiddleware();
