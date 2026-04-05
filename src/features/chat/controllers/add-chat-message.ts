49d21a3afcfccbf592c2e8635d1ca8abe64d8f4f
michael120288 <Michael.sheptun@gmail.com>
2026-04-05 18:14:43 -0400

    Disable chat message email notifications

--- src/features/chat/controllers/add-chat-message.ts
+++ src/features/chat/controllers/add-chat-message.ts
@@ -11,9 +11,6 @@ import { uploads } from '@global/helpers/cloudinary-upload';
 import { BadRequestError } from '@global/helpers/error-handler';
 import { IMessageData, IMessageNotification } from '@chat/interfaces/chat.interface';
 import { socketIOChatObject } from '@socket/chat';
-import { INotificationTemplate } from '@notification/interfaces/notification.interface';
-import { notificationTemplate } from '@service/emails/templates/notifications/notification-template';
-import { emailQueue } from '@service/queues/email.queue';
 import { MessageCache } from '@service/redis/message.cache';
 import { chatQueue } from '@service/queues/chat.queue';
 
@@ -107,20 +104,6 @@ export class Add {
     socketIOChatObject.emit('chat list', data);
   }
 
-  private async messageNotification({ currentUser, message, receiverName, receiverId }: IMessageNotification): Promise<void> {
-    const cachedUser: IUserDocument = (await userCache.getUserFromCache(`${receiverId}`)) as IUserDocument;
-    if (cachedUser.notifications.messages) {
-      const templateParams: INotificationTemplate = {
-        username: receiverName,
-        message,
-        header: `Message notification from ${currentUser.username}`
-      };
-      const template: string = notificationTemplate.notificationMessageTemplate(templateParams);
-      emailQueue.addEmailJob('directMessageEmail', {
-        receiverEmail: cachedUser.email!,
-        template,
-        subject: `You've received messages from ${currentUser.username}`
-      });
-    }
-  }
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  private async messageNotification(_params: IMessageNotification): Promise<void> {}
 }
